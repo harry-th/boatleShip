3f7a283e14e80e770e442b8ed03d909809c3a433
harry-th <42222781+harry-th@users.noreply.github.com>
2022-12-13 15:05:11 -0500

    boat placing and ai boat placing flow

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Board from './Board'
 
 function App() {
-  const generateBoard= () => {
+  const generateBoard = () => {
     const setBoard = () => {
       let answer = {}
       for (let i = 0; i < 100; i++) {
-       answer['a'+i]= ''
+        answer[i] = { state: null, hover: false }
       }
       return answer
     }
@@ -15,18 +15,103 @@ function App() {
     return board
   }
 
- 
- 
+  const [enemyBoatPlacements, setEnemyBoatPlacements] = useState([])
+  const [enemyBoats, setEnemyBoats] = useState([2, 3, 4, 5])
 
 
-const [enemyBoardState, setEnemyBoardState] = useState(generateBoard())
+  const generateTargets = () => {
+    let targets = []
+    let boatPlacementsHolder = null
+    let q = 0;
+    while (q < enemyBoats.length) {
+      let orientationTruth = Math.random() > 0.5
+      let orientation = orientationTruth ? 'h' : 'v'
+      let boundaries = orientationTruth ? Array(10).fill().map((item, i) => i && (i * 10)).filter(item => item) : Array(10).fill().map((item, i) => i + 100)
+      let boatLength = [...enemyBoats][q]
+      let array = []
+      for (const b of boundaries) {
+        for (let i = 1; i < boatLength; i++) {
+          orientationTruth ? array.push(b - i) : array.push(b - i * 10)
+        }
+      }
+      boundaries = boundaries.concat(array)
+
+      if (boatPlacementsHolder) {
+        for (const boatP in boatPlacementsHolder) {
+          if (orientationTruth) {
+            boundaries.push(...boatPlacementsHolder[boatP].positions)
+            for (const pos of boatPlacementsHolder[boatP].positions) {
+              for (let i = 1; i <= boatLength; i++) {
+                boundaries.push(pos - i)
+              }
+            }
+          } else {
+            boundaries.push(...boatPlacementsHolder[boatP].positions)
+            for (const pos of boatPlacementsHolder[boatP].positions) {
+              for (let i = 1; i <= boatLength; i++) {
+                boundaries.push(pos - i * 10)
+              }
+            }
+          }
+        }
+      }
+      let useableBoard = Array(100).fill().map((non, i) => {
+        return i
+      }).filter((item) => {
+        return !boundaries.includes(item)
+      })
+      let index = useableBoard[Math.floor(Math.random() * useableBoard.length)]
+      let positions = Array(boatLength).fill().map((item, i) => {
+        return orientation === 'h' ? index + i : index + i * 10
+      })
+      targets = targets.concat(positions)
+      boatPlacementsHolder = { ...boatPlacementsHolder, [q]: { positions, orientation, length: boatLength } }
+      //   return { ...prev, [prev.length]: { positions, orientation, length: boatLength } }
+      // })
+      q++
+    }
+    // setEnemyBoatPlacements(boatPlacementsHolder)
+    return targets
+  }
+
+  const [orientation, setOrientation] = useState('h')
+  const [boatPlacements, setBoatPlacements] = useState([])
+  const [gameProgress, setGameProgress] = useState('placement')
+  const [boardState, setBoardState] = useState(generateBoard())
+  const [enemyBoardState, setEnemyBoardState] = useState(generateBoard())
+  const [targets, setTargets] = useState([])
+  const [enemyTargets, setEnemyTargets] = useState()
+  const [boats, setBoats] = useState([2, 3, 4, 5])
 
   return (
     <div className="App">
-      <div style={{marginTop:'30px',marginBottom:'30px'}}>WELCOME TO BATTLESHIP</div>
-      <div style={{display:'flex', justifyContent: 'space-evenly'}}>
-        <Board board={generateBoard()} player={'player'} setEnemyBoardState={setEnemyBoardState} enemyBoardState={enemyBoardState}  />
-        <Board player={'ai'} enemyBoardState={enemyBoardState} />
+      <div style={{ marginTop: '30px', marginBottom: '30px' }}>WELCOME TO BATTLESHIP</div>
+      <button onClick={() => { setEnemyTargets(generateTargets()) }}>start game</button>
+      <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <div>
+          <button onClick={() => { orientation === 'v' ? setOrientation('h') : setOrientation('v') }}>
+            change boat orientation
+          </button>
+          <button onClick={() => {
+            let newBoats = [...boats]
+            newBoats.unshift(boatPlacements[boatPlacements.length - 1].length)
+            setBoatPlacements(prev => prev.slice(0, prev.length - 1))
+            setBoats(newBoats)
+          }}>
+            unplace the last boat
+          </button>
+        </div>
+        <Board board={generateBoard()} player={'player'} boardState={boardState}
+          setBoardState={setBoardState} enemyTargets={enemyTargets}
+          enemyBoardState={enemyBoardState} boatPlacements={boatPlacements}
+          setBoatPlacements={setBoatPlacements} boats={boats} setBoats={setBoats}
+          orientation={orientation} gameProgress={gameProgress} setGameProgress={setGameProgress}
+          targets={targets} setTargets={setTargets} />
+        <Board player={'ai'} enemyBoardState={enemyBoardState}
+          setEnemyBoardState={setEnemyBoardState} boardState={boardState} setBoardState={setBoardState}
+          gameProgress={gameProgress} setGameProgress={setGameProgress}
+          targets={targets} setTargets={setTargets}
+          enemyTargets={enemyTargets} setEnemyTargets={setEnemyTargets} />
       </div>
     </div>
   );
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,70 +1,121 @@
-import React, {useState } from 'react'
-import styles from './Board.module.css'
-const  Board = ({player, enemyBoardState, setEnemyBoardState, board}) => { 
-
-const generateTargets = () => {
-  let targets = []
-  let i = 0;
-  while (i<10) {
-    targets.push(Math.floor(Math.random()*100))
-    i++
-  }
-  return targets
-}
+import React, { useState } from 'react'
+import styles from './styles/Board.module.css'
+const Board = ({ player, boardState, setBoardState, enemyBoardState, setEnemyBoardState,
+  targets, setTargets, enemyTargets, setEnemyTargets, orientation, setBoatPlacements, boats, setBoats,
+  gameProgress, setGameProgress }) => {
 
 
-  const [boardState, setBoardState] = useState(board)
   const [yourHits, setHits] = useState(0)
   const [enemyHits, setEnemyHits] = useState(0)
-const [targets, setTargets] = useState(generateTargets())
-console.log(targets)
 
-
-let aiAttack = () => {
-  let enemyAttack = Math.floor(Math.random()*100)
-  let hitOrMiss = Object.values(targets).includes(enemyAttack)
-  let state = hitOrMiss ? 'hit!' : 'missed!'
-  // console.log(enemyBoardState)
-  let newState = {...enemyBoardState}
-  newState['a'+enemyAttack] = state
-  setEnemyBoardState(newState)
-  if (hitOrMiss) {
-    setEnemyHits((enemyHits)=>enemyHits+1)
-    if(enemyHits>=10) alert('you LOSE')
-
-    alert('you got HIT!')
-  }
-}
-
-
-  const checkHit = (index) => {
-    let hitOrMiss = Object.values(targets).includes(index)
-    // console.log(hitOrMiss, index, targets)
-    let state = hitOrMiss ? 'hit!' : 'missed!'
-    let newState = {...boardState}
-    newState['a'+index] = state
+  let aiAttack = () => {
+    let unknownSquare = []
+    for (const square in boardState) {
+      if (boardState[square].state !== 'hit' && boardState[square].state !== 'missed') {
+        unknownSquare.push((square))
+      }
+    }
+    let enemyAttack = unknownSquare[Math.floor(Math.random() * unknownSquare.length)]
+    let hitOrMiss = (targets).includes(Number(enemyAttack))
+    console.log(enemyAttack, hitOrMiss, targets, unknownSquare)
+    let state = hitOrMiss ? 'hit' : 'missed'
+    let newState = { ...boardState }
+    newState[enemyAttack] = { state, hover: false }
     setBoardState(newState)
     if (hitOrMiss) {
-    setHits((hits)=>hits+1)
-    // console.log(yourHits)
-    if(yourHits>8) alert('you Win')
-      alert('Nice Shot!')
+      setEnemyHits((enemyHits) => enemyHits + 1)
+      if (enemyHits >= 5) setGameProgress('YOU LOSE')
+      alert('you got HIT!')
     }
-    aiAttack()
   }
-// console.log(boardState['a0'])
-    let element = (index) => { 
-      let squareID = ''
-      player === 'player' ? squareID = boardState['a'+index] : squareID = enemyBoardState['a'+index]
-      return <div key={index} style={{'backgroundColor':  squareID === 'missed!' ? 'white' : squareID === 'hit!' ? 'red' : 'blue', 'pointerEvents': squareID ==='hit!' ||squareID ==='missed!' ? 'none' : 'auto'}}
-  onClick={()=>checkHit(index)}
-  className={styles.square}
-  >
+
+  const checkHit = (index) => {
+    if (gameProgress === 'placement') {
+      if (boats.length === 1) {
+        setGameProgress('ongoing')
+      }
+      let num = [...boats].shift()
+      let positions = Array(num).fill().map((item, i) => {
+        return orientation === 'h' ? index + i : index + i * 10
+      })
+      if (positions.some((pos) => targets.includes(pos))) return
+      if ((Math.floor(positions[positions.length - 1] / 10) * 10) - (Math.floor(positions[0] / 10) * 10) > 0) return
+      setTargets(p => [...p, ...positions])
+      setBoatPlacements(prev => {
+        return ({ ...prev, [prev.length]: { positions, orientation, length: num } })
+      })
+      let newBoardState = { ...boardState }
+      for (const square in newBoardState) {
+        if (positions.includes(Number(square))) {
+          newBoardState[square].state = 'mine'
+        }
+      }
+      setBoardState(newBoardState)
+      setBoats(prev => {
+        return prev.slice(1, prev.length)
+      })
+    } else {
+      let hitOrMiss = enemyTargets.includes(index)
+      let state = hitOrMiss ? 'hit' : 'missed'
+      let newState = { ...enemyBoardState }
+      newState[index] = { state, hover: false }
+      setEnemyBoardState(newState)
+      if (hitOrMiss) {
+        setHits((hits) => hits + 1)
+        if (yourHits >= 5) setGameProgress('YOU WON')
+        alert('Nice Shot!')
+      }
+      aiAttack()
+    }
+  }
+
+
+
+  let element = (index) => {
+    let boardClass = player === 'player' ? boardState : enemyBoardState
+
+    return <div key={index}
+      onClick={() => checkHit(index)}
+
+      onMouseEnter={() => {
+        if (gameProgress === 'placement' && boardState) {
+          let coords = []
+          for (let i = 0; i < boats[0]; i++) {
+            coords.push(orientation === 'h' ? index + i : index + i * 10)
+          }
+          let newBoardState = { ...boardState }
+          for (let i = 0; i < coords.length; i++) {
+            if (boardState[coords[i]].state === 'mine') return
+          }
+          // console.log((Math.floor(coords[coords.findIndex((r) => r === square)] / 10) * 10), (Math.floor(coords[0] / 10) * 10))
+
+          for (const square in newBoardState) {
+            if (coords.includes(Number(square)) && ((Math.floor(coords[coords.findIndex((r) => r === square) + 1] / 10) * 10) - (Math.floor(square / 10) * 10) === 0)) {
+              newBoardState[square].hover = 'hover'
+            } else if (newBoardState[square].hover === 'hover') {
+              newBoardState[square].hover = false
+            }
+          }
+          setBoardState(newBoardState)
+        }
+      }
+      }
+      className={[styles.square,
+      boardClass && styles[(boardClass)[index].state],
+      boardClass && styles[(boardClass)[index].hover]
+      ].join(' ')
+      }>
       {index}
-      </div>}
+    </div >
+  }
+
   return (
-    <div style={{width:'400px', height:'400px', 'pointerEvents': player==='player' ? 'auto' : 'none'}}>
-       {[...Array(100)].map((e, i) => <>{element(i)}</>)}
+    <div>
+      <button onClick={() => { console.log(targets, enemyTargets) }}>print</button>
+      {(gameProgress === 'YOU WON' || gameProgress === 'YOU LOSE') ? <p>{gameProgress}</p> : ""}
+      <div className={styles.board}>
+        {[...Array(100)].map((e, i) => <>{element(i)}</>)}
+      </div>
     </div>
   );
 }
