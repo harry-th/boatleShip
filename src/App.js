97ddba2b2d3b26ab289706341d3c3d2ee8b27983
harry-th <42222781+harry-th@users.noreply.github.com>
2023-01-06 16:44:34 -0500

    m

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ function App() {
   //websocket connection
   useEffect(() => {
     if (Object.keys(cookies).length === 0) setCookie('user', { id: randomstring.generate(), name: 'noName', state: 'matching', wins: 0, losses: 0 })
-    const newSocket = new WebSocket('wss:// 3.14.176.234 :8080')
+    const newSocket = new WebSocket('wss:// 3.14.176.234:8080')
     // new WebSocket('ws://localhost:8080/ws');
     newSocket.onmessage = (event) => {
       let message = JSON.parse(event.data);
